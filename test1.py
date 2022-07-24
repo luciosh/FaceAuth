19aa502451ae102c39ffeadab107af86159bd16b
Sergio Lucio <45217959+Lucioid@users.noreply.github.com>
2022-07-24 11:59:45 -0400

    ADD - Desenho oval e deteccao de nariz no Facedetect.py

--- test1.py
+++ test1.py
@@ -1,8 +1,8 @@
-
 import cv2
 
 webcam = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier("cascade.xml")
+face_cascade = cv2.CascadeClassifier("face-cascade.xml")
+nose_cascade = cv2.CascadeClassifier("nose-cascade.xml")
 
 center_coordinates = (120, 100)
 axesLength = (100, 50)
@@ -14,7 +14,7 @@ color = (0, 0, 255)
 
 # Line thickness of 5 px
 thickness = 5
-print("\n\nPress 'esc' to quit program\n\n");
+print("\n\nPress 'esc' to quit program\n\n")
 while (True):
     s, video = webcam.read()
 
@@ -24,18 +24,28 @@ while (True):
                                           minNeighbors=20,
                                           minSize=(30, 30),
                                           maxSize=(400, 400))
-    print(faces);                                          
+    # print(faces)
     for (x, y, w, h) in faces:
         # cv2.circle(video, (x, y), (x+w, y+h), (0, 255, 0), 4)
         # cv2.ellipse(image, center_coordinates, axesLength, angle, startAngle, endAngle, color, thickness)
-        cv2.ellipse(video, (x+(int(w/2)), y+(int(h/2))), (w, h), angle, startAngle, endAngle,
+        cv2.ellipse(video, (x + (int(w / 2)), y + (int(h / 2))), (round(w*0.45), round(h*0.6)), angle,
+                    startAngle, endAngle, (0, 255, 0), 4)
+        window_name = 'Image'
+
+    nose = nose_cascade.detectMultiScale(video,
+                                         minNeighbors=20,
+                                         minSize=(30, 30),
+                                         maxSize=(400, 400))
+    for (x, y, w, h) in nose:
+        # cv2.circle(video, (x, y), (x+w, y+h), (0, 255, 0), 4)
+        # cv2.ellipse(image, center_coordinates, axesLength, angle, startAngle, endAngle, color, thickness)
+        cv2.ellipse(video, (x+(int(w/2)), y+(int(h/2))), (round(w/2), round(h/2)), angle, startAngle, endAngle,
                     (0, 255, 0), 4)
         window_name = 'Image'
 
     cv2.imshow("Face Detection", video)
 
-
-    if (cv2.waitKey(1)==27):
+    if (cv2.waitKey(1) == 27):
         break
 webcam.release()
 cv2.destroyAllWindows()
